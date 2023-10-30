068d66edca28a88cbb8a07a4683043c98bd5f5c5
Katarzyna <noworyta.katarzyna@wp.pl>
2023-10-30 16:47:33 +0100

    RestTemplateBuilder Configuration

--- src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
+++ src/main/java/guru/springframework/spring6resttemplate/client/BeerClientImpl.java
@@ -19,30 +19,14 @@ public class BeerClientImpl implements BeerClient {
 
     private final RestTemplateBuilder restTemplateBuilder;
 
-    private static final String BASE_URL = "http://localhost:8080";
     private static final String GET_BEER_PATH = "/api/v1/beer";
 
     @Override
     public Page<BeerDTO> listBeers() {
         RestTemplate restTemplate = restTemplateBuilder.build();
 
-/*        ResponseEntity<String> stringResponse = restTemplate
-                .getForEntity(BASE_URL + GET_BEER_PATH, String.class);
-
-        ResponseEntity<Map> mapResponse = restTemplate
-                .getForEntity(BASE_URL + GET_BEER_PATH, Map.class);
-
-        ResponseEntity<JsonNode> jsonResponse = restTemplate
-                .getForEntity(BASE_URL + GET_BEER_PATH, JsonNode.class);
-
-        jsonResponse.getBody().findPath("content")
-                        .elements().forEachRemaining(node ->
-                        System.out.println(node.get("beerName").asText()));
-
-        System.out.println(stringResponse.getBody());*/
-
         ResponseEntity<BeerDTOPageImpl> stringResponse =
-                restTemplate.getForEntity(BASE_URL + GET_BEER_PATH , BeerDTOPageImpl.class);
+                restTemplate.getForEntity(GET_BEER_PATH , BeerDTOPageImpl.class);
 
         return null;
     }
