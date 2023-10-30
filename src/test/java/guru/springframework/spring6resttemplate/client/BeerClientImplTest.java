1f5406faa8e461e11a3b17550d3c9dde1267c046
Katarzyna <noworyta.katarzyna@wp.pl>
2023-10-30 17:14:28 +0100

    Query Parameters

--- src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
+++ src/test/java/guru/springframework/spring6resttemplate/client/BeerClientImplTest.java
@@ -13,8 +13,12 @@ class BeerClientImplTest {
     BeerClientImpl beerClient;
 
     @Test
-    void listBeers(){
-        beerClient.listBeers();
+    void listBeersNoBeerName(){
+        beerClient.listBeers(null);
     }
 
+    @Test
+    void listBeers(){
+        beerClient.listBeers("ALE");
+    }
 }
